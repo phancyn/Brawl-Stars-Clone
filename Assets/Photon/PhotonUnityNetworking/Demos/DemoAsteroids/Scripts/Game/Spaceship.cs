94d312daa9786b2ae224d7b0da2896465ad73372
phancyn✨ <shvalinc@gmail.com>
2024-11-06 15:57:11 +0500

    Let's go

--- Assets/Photon/PhotonUnityNetworking/Demos/DemoAsteroids/Scripts/Game/Spaceship.cs
+++ Assets/Photon/PhotonUnityNetworking/Demos/DemoAsteroids/Scripts/Game/Spaceship.cs
@@ -101,9 +101,9 @@ namespace Photon.Pun.Demo.Asteroids
             Vector3 force = (rot * Vector3.forward) * acceleration * 1000.0f * MovementSpeed * Time.fixedDeltaTime;
             rigidbody.AddForce(force);
 
-            if (rigidbody.velocity.magnitude > (MaxSpeed * 1000.0f))
+            if (rigidbody.linearVelocity.magnitude > (MaxSpeed * 1000.0f))
             {
-                rigidbody.velocity = rigidbody.velocity.normalized * MaxSpeed * 1000.0f;
+                rigidbody.linearVelocity = rigidbody.linearVelocity.normalized * MaxSpeed * 1000.0f;
             }
 
             CheckExitScreen();
@@ -127,7 +127,7 @@ namespace Photon.Pun.Demo.Asteroids
         [PunRPC]
         public void DestroySpaceship()
         {
-            rigidbody.velocity = Vector3.zero;
+            rigidbody.linearVelocity = Vector3.zero;
             rigidbody.angularVelocity = Vector3.zero;
 
             collider.enabled = false;
